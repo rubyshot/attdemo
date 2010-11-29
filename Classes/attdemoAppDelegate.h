88d9aa1a16dc156902505415ab4254f5108fb7bb
Ruby THE <domochoice@yahoo.com>
2010-11-28 17:23:27 -0700

    2nd commit

---
+++ Classes/attdemoAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  attdemoAppDelegate.h
+//  attdemo
+//
+//  Created by Preferred Customer on 11/28/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface attdemoAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
